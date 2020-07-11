037b75a5b011641c1eb6df5f08c09e36c260f886
Joao Antonio Cabral <joaomunizshura@gmail.com>
2020-07-11 10:39:40 -0300

    Ajuste de pacotes para o pacote controller.

--- src/ControlePraia/controller/ControllerAtendente.java
+++ src/ControlePraia/controller/ControllerAtendente.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ControlePraia.model.view.controller;
+package ControlePraia.controller;
 
 /**
  *
  * @author PC
  */
-public class ControllerAcampamento {
+public class ControllerAtendente {
     
 }
